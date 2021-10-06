6a31850dfe349cf75abb0cc2825d7b6e863f0f84
Lucas <lucasmbrute614@gmail.com>
2021-10-05 22:14:35 -0300

    form POST & EDIT

--- poke-api/my-app/src/Api/Api.js
+++ poke-api/my-app/src/Api/Api.js
@@ -1,6 +1,11 @@
 export const Api = {
+    
+    // ENTITY -- USER--
     baseUrl: "http://localhost:3000",
 
     readAllUrl: () => fetch(Api.baseUrl + "/user"),
     readById: (id)=> fetch(Api.baseurl+"/user/"+id),
+    
+    //POST
+    // postUrl: (id,body)=>fetch(Api.baseUrl+"/user")
 }
--- poke-api/my-app/src/Pages/Create/Create.js
+++ poke-api/my-app/src/Pages/Create/Create.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
 export default function Create() {
+    
+    const handleSubmit = (e)=>e.preventDefault();
+    
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='name'>Title: </label>
+                <br/>
+                <input type='text' name='name' id = 'name'></input>
+                <label htmlFor='lastName'></label> 
+                <br/>
+                <input type='text' name='lastName' id='lastName'></input>
+                <label htmlFor='password'></label>
+                <br/>
+                <input type='text' name='password' id='password'></input>
+                <label htmlFor='email'></label>
+                <br/>
+                <input type='text' name='email'id='email'></input>
+                <label htmlFor="cpf"></label>
+                <br/>
+                <input type='text' name='cpf' id='cpf'></input>
+                
+                <input type='submit' value='add'></input>
+            </form>
+
         </div>
     )
 }
